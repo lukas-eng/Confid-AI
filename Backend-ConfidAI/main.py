5c8e737523dade99e493fe0c1cc480adaf4cd6f0
Nicol Dayyana Cabrera Nieto <nicolcabrera007@gmail.com>
2026-02-06 10:09:04 -0500

    Add files via upload

--- Backend-ConfidAI/main.py
+++ Backend-ConfidAI/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from config.database import Base, engine
 from routes.user_routes import router as user_router
 from routes.auth_routes import router as auth_router
-
+from fastapi.middleware.cors import CORSMiddleware
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
@@ -13,3 +13,16 @@ app.include_router(user_router, prefix="/usuarios", tags=["Usuarios"])
 
 # API Iniciar sesion 
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+# REGISTRAR RUTAS
+app.include_router(user_router)
