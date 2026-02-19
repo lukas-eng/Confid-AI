a63210b30841625b9f65ddf30157b2b643fe43b6
Lucas Garcia Niño <lukasnino737@gmail.com>
2026-02-19 09:55:35 -0500

    sprint 3

--- Backend-ConfidAI/main.py
+++ Backend-ConfidAI/main.py
@@ -17,8 +17,8 @@ app.include_router(auth_router, prefix="/auth", tags=["Auth"])
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://localhost:3001",
+        "http://127.0.0.1:3001"
     ],
     allow_credentials=True,
     allow_methods=["*"],
